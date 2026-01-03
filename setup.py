1036589ea27d87198ad8ba64813627ed57c30e30
Dean Benjamin Anderson <dben1182@gmail.com>
2026-01-02 19:35:08 -0700

    first commit

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'scipy'],
     zip_safe=True,
     maintainer='benjamin',
     maintainer_email='dben1182@gmail.com',
